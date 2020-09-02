4a0a487554a06729f4c766ec4a68e55b0285fb0a
ehsankhodayar <ehsankhodayar349@gmail.com>
2020-09-02 19:23:42 +0430

    Cloud Federation -Changing the federation policy in DatacenterBrokerMainFFD.java

--- cloudsim-plus-examples/src/main/java/org/myPaper/broker/DatacenterBrokerMainFFD.java
+++ cloudsim-plus-examples/src/main/java/org/myPaper/broker/DatacenterBrokerMainFFD.java
@@ -7,22 +7,9 @@ import org.cloudbus.cloudsim.vms.Vm;
 import org.myPaper.acsAlgorithms.DatacenterSolutionEntry;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class DatacenterBrokerMainFFD extends DatacenterBrokerMain {
-
-    /**
-     * Creates a DatacenterBroker giving a specific name.
-     * Subclasses usually should provide this constructor and
-     * and overloaded version that just requires the {@link CloudSim} parameter.
-     *
-     * @param simulation the CloudSim instance that represents the simulation the Entity is related to
-     * @param name       the DatacenterBroker name
-     */
-    public DatacenterBrokerMainFFD(CloudSim simulation, String name) {
-        this(simulation, name, new ArrayList<>());
-    }
 
     /**
      * Creates a DatacenterBroker giving a specific name.
@@ -48,8 +35,7 @@ public class DatacenterBrokerMainFFD extends DatacenterBrokerMain {
         }
 
         //Look for suitable resources inside the datacenters of current cloud provider
-        LOGGER.info("{}: {} is trying to find suitable resources for allocating to the new Vm creation requests inside the datacenters of the " +
-                "current cloud provider.",
+        LOGGER.info("{}: {} is trying to find suitable resources for allocating to the new Vm creation requests inside the available datacenters.",
             getSimulation().clockStr(),
             getName());
 
@@ -57,7 +43,7 @@ public class DatacenterBrokerMainFFD extends DatacenterBrokerMain {
 
         vmLoop:
         for (Vm vm : getVmWaitingList()) {
-            for (Datacenter datacenter : getProviderDatacenters()) {
+            for (Datacenter datacenter : getDatacenterList()) {
 
                 for (Host host : getAllowedHostList(datacenter)) {
                     if (host.isSuitableForVm(vm)) {
@@ -80,58 +66,13 @@ public class DatacenterBrokerMainFFD extends DatacenterBrokerMain {
 
             return true;
         } else {
-            LOGGER.warn("{}: {} could not find suitable resources for all the new Vm creation requests inside the datacenters " +
-                    "of the current cloud provider!",
+            LOGGER.warn("{}: {} could not find suitable resources for all the new Vm creation requests inside the available datacenters!",
                 getSimulation().clockStr(),
                 getName());
 
-            //Look for suitable resources inside the federated datacenters
-            List<Datacenter> federatedDcList = getFederatedDatacenters();
+            failVms(vmList);
 
-            if (federatedDcList.isEmpty()) {
-                failVms(vmList);
-
-                return false;
-            } else {
-                LOGGER.info("{}: {} is trying to federate some of the new Vm creation requests.",
-                    getSimulation().clockStr(),
-                    getName());
-
-                vmLoop:
-                for (Vm vm : vmList) {
-                    for (Datacenter federatedDc : federatedDcList) {
-
-                        for (Host host : getAllowedHostList(federatedDc)) {
-                            if (host.isSuitableForVm(vm)) {
-                                LOGGER.info("{}: {} has found a suitable resource for a new Vm creation request inside " +
-                                        "the federated environment.",
-                                    getSimulation().clockStr(),
-                                    getName());
-
-                                vm.setHost(host);
-                                host.createTemporaryVm(vm);
-                                vmList.remove(vm);
-                                this.vmCreationRequests += requestVmCreation(federatedDc, isFallbackDatacenter, vm);
-
-                                continue vmLoop;
-                            }
-                        }
-                    }
-                }
-
-                if (vmList.isEmpty()) {
-                    return true;
-                }else {
-                    LOGGER.warn("{}: {} could not find any suitable resource for some of the new Vm creation requests " +
-                            "inside the federated environment!",
-                        getSimulation().clockStr(),
-                        getName());
-
-                    failVms(vmList);
-
-                    return false;
-                }
-            }
+            return false;
         }
     }
 
